0836440309a0d6deda377871f11b86f885fadc36
Jieyu <nicolewang404@outlook.com>
2022-08-01 00:47:54 +0800

    delete api

--- src/main/java/com/example/demoapi/CompanyController.java
+++ src/main/java/com/example/demoapi/CompanyController.java
@@ -1,11 +1,8 @@
 package com.example.demoapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 //@Controller
@@ -24,15 +21,26 @@ public class CompanyController {
 
     @RequestMapping(value = "/companies", method = RequestMethod.POST)
 
-    public void addCompany(@RequestBody Company company) {
-        companyService.createCompany(company); //post有请求体 get没有
+    public boolean addCompany(@RequestBody Company company) {
+        return companyService.createCompany(company); //post有请求体 get没有
     }
 
     @RequestMapping(value = "/companies/{id}", method = RequestMethod.GET)
 
-    public Company getCompany(@PathVariable(value="id") Integer id) {
+    public Company getCompany(@PathVariable(value = "id") Integer id) {
         return companyService.getCompany(id);
 
+    }
+
+//    @RequestMapping(value = "/companies/{id}", method = RequestMethod.PUT)
+//
+//    public Company updateCompany(@PathVariable(value = "id") Company company) {
+//        return companyService.updateById();
+
+    @RequestMapping(value = "/companies/{id}", method = RequestMethod.DELETE)
+
+    public boolean deleteCompany(@PathVariable(value = "id") Integer id) {
+        return companyService.deleteCompany(id);
     }
 }
 
--- src/main/java/com/example/demoapi/CompanyService.java
+++ src/main/java/com/example/demoapi/CompanyService.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class CompanyService {
 
     private final Map<Integer, Company> companyMap = new HashMap<>();
+
     public CompanyService() {
         companyMap.put(1, new Company(1, "AAA", 22));
         companyMap.put(2, new Company(2, "BBB", 33));
@@ -20,12 +21,22 @@ public class CompanyService {
         return new ArrayList<>(companyMap.values());
     }
 
-    public void createCompany( Company company) {
+    public boolean createCompany(Company company) {
         companyMap.put(company.getId(), company);
+        return true;
     }
 
-    public Company getCompany (Integer id) {
+    public Company getCompany(Integer id) {
         return companyMap.get(id);
     }
 
-}
+    public boolean deleteCompany(Integer id) {
+        companyMap.remove(id);
+        return true;
+    }
+
+
+//    public Company updateById(Company company) {
+//Company updateCompany = company.getId();
+    }
+
