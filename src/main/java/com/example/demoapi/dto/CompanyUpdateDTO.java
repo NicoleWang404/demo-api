cac6ea31a9683bc7d9d3a35a9640fee0d59f2229
Jieyu <nicolewang404@outlook.com>
2022-10-14 01:20:42 +0800

    fix:optimize company property

--- src/main/java/com/example/demoapi/dto/CompanyUpdateDTO.java
+++ src/main/java/com/example/demoapi/dto/CompanyUpdateDTO.java
@@ -1,24 +1,18 @@
 package com.example.demoapi.dto;
 
-import com.example.demoapi.entities.Employee;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CompanyDTO {
+public class CompanyUpdateDTO {
     @NotNull
     private String companyName;
     @NotNull
-    private Integer employeesNumber;
-    @NotNull
-    private List<Employee> employees;
-
+    private String location;
 
 }
